92a44ba6bc6488e0a30252c8f2e9c380210ac173
LeonardWiidh <leonard.wiidh@elev.ga.ntig.se>
2023-08-24 12:46:47 +0200

    wab01

---
+++ intro 1.py
@@ -0,0 +1,31 @@
+#1. Hello World
+print("hello world")
+#2. Printa ut en text och ha med ett citat
+print("'hello world'")
+#3. Skriv “Lorem Ipsum” tio gånger under varandra med trippel citattecken
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+""")
+#4. Skriv flera saker i en och samma print() med \n
+print("hej hej \n hejdå hejdå")
+#5. Sammanfoga strängar med hjälp av plus-parametern: print(“Fy “ + “17!”),
+print("Fy " + "fan")
+#6. Sammanfoga text och variabel med kommatecken (,) 
+print("amogos","mogosgos")
+#7. Ta in data genom input och skriva ut på skärmen
+vadhangor = input("heloink vad gör han ")
+print("han gör " + vadhangor)
+#8. Välkomstprogram med input - ha med ditt namn på något sätt
+print("hej och välkomen till mitt program")
+namn = input("vad heter du ")
+age = input("hur gammal är du ")
+print("hej " + namn + " du är " + age + " år gammal")
